494041884fcd23a3e5b2088dde9c67ad2a95a285
Aadhitya Kumar <axk8897@mavs.uta.edu>
2023-08-28 20:35:23 -0500

    P01

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,10 +1,15 @@
 import java.net.*;
+//import java.rmi.UnknownHostException;
 
-public class Name {
+public class Hello {
     public static void main(String[] args) {
-    
+    try{
     InetAddress my_address = InetAddress.getLocalHost();
     System.out.println("Hello, " + my_address.getHostName());
     System.out.println();
+    }
+    catch (UnknownHostException e){
+        System.out.println( "Could not find host name.");
+    }
     }
     }
