01305ab34b0b22f9896bf307fa0ee21eb01ec587
Aadhitya Kumar <axk8897@mavs.uta.edu>
2023-09-04 14:53:28 -0500

    P02

--- P02/full_credit/TestLine.java
+++ P02/full_credit/TestLine.java
@@ -2,8 +2,14 @@ class TestLine
 {
     public static void main(String[] args)
         {
-            Colors clr = new Colors(Color.ORANGE, 1.0, 2.0, 5.0, 6.0);
+            Colors clr1 = new Colors(Color.ORANGE, 1.0, 2.0, 5.0, 6.0);
+            Colors clr2 = new Colors(Color.PURPLE, 3.0, 4.0, 7.0, 8.0);
+            Colors clr3 = new Colors(Color.PINK, 0.0, 0.0, 3.0, 4.0);
+            Colors clr4 = new Colors(Color.BROWN, 2.5, 2.5, 6.5, 6.5);
 
-            System.out.println(clr);
+            System.out.println(clr1);
+            System.out.println(clr2);
+            System.out.println(clr3);
+            System.out.println(clr4);
         }
 }
