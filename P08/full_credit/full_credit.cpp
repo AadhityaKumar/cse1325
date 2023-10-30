18b9264357c212eb76db295d57ac97e8fa57ef3e
Aadhitya Kumar <axk8897@mavs.uta.edu>
2023-10-30 09:57:37 -0500

    P08

---
+++ P08/full_credit/full_credit.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+std::cout << "Hello, Aadhitya! \nWhat is your name?" << std::endl;
+std::string name;
+std::cin >> name;
+std::cout << "Hello, " << name << "!" << std::endl;
+} 
